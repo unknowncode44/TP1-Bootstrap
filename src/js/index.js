206261f8146b35063526eb6396f39e0ac1e7754c
matiaz.orellana <matiaz.orellana@icloud.com>
2022-05-28 16:31:32 -0300

    220528

---
+++ src/js/index.js
@@ -0,0 +1,54 @@
+
+new fullpage('#fullpage', {
+    autoscrolling: true,
+    scrollHorizontally: true,
+    parallax: true,
+    sectionsColor: ['', '', 'whitesmoke','#f2f2f2'],
+    anchors:['home', 'products', 'services', 'about'],
+    
+})
+
+function stars(){
+    let count   = 30; // definimos la cantidad de estrellas
+
+    // elegimos la clase que representara el espacio 
+    let scene   = document.querySelector('.header-custom-section'); 
+    let i       = 0 // definimos un item acumulable
+
+    // ## Este while va a crear estrellas que van a aparecer en 
+    // distintas ubicaciones a lo largo de la pantalla
+
+    while(i < count){ // va a crear la misma cantidad que 'count'
+
+        // creamos el elemento que sera una simple estrella
+        let star        = document.createElement('i');  
+        // a lo largo del ancho de la pantalla
+        let x           = Math.floor(Math.random()*window.innerWidth*2);
+        
+        // duracion del efecto
+        let duration    = Math.random() * 1;
+        // 
+        let h           = Math.random() * 80;
+
+        //## Definimos un estilo para el elemento i
+
+        // para ver distintas ubicaciones
+        star.style.left     = x + 'px';
+        // el ancho siempre sera 1px
+        star.style.width    = 2 + 'px';
+        // el alto sera random
+        star.style.height   = h + 'px';
+        // asignamos valor de duracion al atributo animationDuration
+        star.style.animationDuration = duration + 's'
+
+
+
+        // embebemos el elemento i que creamos dentro de scene.
+        scene.appendChild(star)
+        // aumentamos uno al contador para completar un ciclo
+        i++
+
+    }
+    
+}
+stars()
